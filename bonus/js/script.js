b85ad6214b3cdcaddcc065479cd1d88460c376df
Flavio <flavio.alessandri@gmail.com>
2020-06-25 17:13:10 +0200

    Css - bonus

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -6,12 +6,12 @@ var listLength = 100;  //list lenght (first index = 0; 100 = from 0 to 99)
 for(i=0; i <listLength + 1; i++){
   lista.push(i); // at the end of every "for" cycle add the numbers in the list
   if (i%5 == 0 && i%3 == 0){ // get the number divisible by 5 and by 3!
-  lista[i] = "Fizz-Buzz";
+  lista[i] = "<i class='fas fa-user-graduate'></i>"+ "FizzBuzz";
 } else if (i%3 == 0 && i%5 != 0 ){ // get only the number divisible by 3!
-  lista[i] = "Fizz";
+  lista[i] = "<i class='fas fa-user-alt'></i>"+ "Fizz";
 } else if (i%5 == 0 && i%3 != 0 ){// get only the number divisible by 5!
-  lista[i] = "Buzz";
+  lista[i] = "<i class='fas fa-graduation-cap'></i>"+ "Buzz";
 }
-document.getElementById('fizzBuzz').innerHTML += "<li>" +[i] + " ) "+ lista[i] +  "</li>";//fill the list
+document.getElementById('fizzBuzz').innerHTML += "<li>"+ lista[i] +  "</li>";//fill the list
 console.log(lista);//consol.log list
 }
